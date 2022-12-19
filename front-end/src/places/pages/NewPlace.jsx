892f319fdd72dc86cdf3b556ce7a05d90ccd826e
elene638 <elenejavaxishvili@yahoo.com>
2022-12-19 03:49:55 -0800

    Add input validation

--- front-end/src/places/pages/NewPlace.jsx
+++ front-end/src/places/pages/NewPlace.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "./newPlace.css";
 import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE } from "../../shared/components/Util/validators";
 
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input element="input" type="text" label="Title" />
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+      />
     </form>
   );
 };
--- front-end/src/shared/components/FormElements/Input.jsx
+++ front-end/src/shared/components/FormElements/Input.jsx
@@ -1,17 +1,85 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./input.css";
+import { validate } from "../../../shared/components/Util/validators.js";
 
-const Input = ({ id, label, element, type, placeholder, row }) => {
+const initailState = {
+  value: "",
+  isValid: false,
+  isTouch: false,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE":
+      return {
+        ...state,
+        value: action.value,
+        isValid: validate(action.value, action.validators),
+      };
+    default:
+      return state;
+    case "TOUCH":
+      return {
+        ...state,
+        isTouch: true,
+      };
+  }
+};
+
+const Input = ({
+  id,
+  label,
+  element,
+  type,
+  placeholder,
+  row,
+  errorText,
+  validators,
+}) => {
+  const [state, dispatch] = useReducer(reducer, initailState);
+
+  //   console.log(state);
+
+  const handleTouch = () => {
+    dispatch({ type: "TOUCH" });
+  };
+
+  const handleChange = (event) => {
+    dispatch({
+      type: "CHANGE",
+      value: event.target.value,
+      validators: validators,
+    });
+  };
+
   const elements =
     element === "input" ? (
-      <input id={id} type={type} placeholder={placeholder} />
+      <input
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        onChange={handleChange}
+        value={state.value}
+        onBlur={handleTouch}
+      />
     ) : (
-      <textarea id={id} row={row || 3} />
+      <textarea
+        id={id}
+        row={row || 3}
+        onChange={handleChange}
+        value={state.value}
+        onBlur={handleTouch}
+      />
     );
   return (
-    <div className={`form-control`}>
+    <div
+      className={`form-control ${
+        !state.isValid && state.isTouch && "form-control--invalid"
+      }`}
+    >
       <label htmlFor={id}>{label}</label>
       {elements}
+      {!state.isValid && state.isTouch && <p>{errorText}</p>}
     </div>
   );
 };
