6b20e2b2642a76441f686f9b8bd9e34abd06338d
elene638 <elenejavaxishvili@yahoo.com>
2023-01-11 04:20:39 -0800

    Create user schema and model

---
+++ back-end/models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  places: { type: String, required: true },
+  //unique make it possible to excess to email by giving unique codes and getting them, and package unique validator check if email already exists
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 6 },
+  image: { type: String, required: true },
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("User", userSchema);
